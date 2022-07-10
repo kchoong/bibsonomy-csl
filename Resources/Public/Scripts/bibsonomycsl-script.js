dca3a404fd6520e6b329ce2c4e25560f8dfbed19
Kevin Choong <choong.kvn@gmail.com>
2022-07-10 18:55:09 +0200

    filter posts

--- Resources/Public/Scripts/bibsonomycsl-script.js
+++ Resources/Public/Scripts/bibsonomycsl-script.js
@@ -5,3 +5,29 @@ function toggleSnippet(snippetBtn) {
     $(postId + ' .bibsonomy-snippet:not(' + snippetId + ')').hide(0);
     $(snippetId).toggle(0);
 }
+
+function filterPosts() {
+    let input = $('#bibsonomyInlineFilter');
+    let search = input.val();
+
+    if (search) {
+        $('.bibsonomy-group-header').hide(0);
+        $('.bibsonomy-post').each(function(index) {
+            let postTxt = $(this).text().toLowerCase();
+            if (postTxt.includes(search.toLowerCase())) {
+                $(this).show(0);
+            } else {
+                $(this).hide(0);
+            }
+        });
+    } else {
+        $('.bibsonomy-group-header').show(0);
+        $('.bibsonomy-post').show(0);
+    }
+}
+
+function resetFilterPosts() {
+    $('#bibsonomyInlineFilter').val('');
+    $('.bibsonomy-group-header').show(0);
+    $('.bibsonomy-post').show(0);
+}
